5526f433c55f5506a05d002170621d3895463883
Gabriel Prado <gaboalespana@gmail.com>
2025-07-13 23:53:29 -0400

    update

--- src/actions/match-actions.ts
+++ src/actions/match-actions.ts
@@ -1,13 +1,39 @@
 "use server";
 
 import { postMatch } from "@/db/methods/match";
-import { generateGroupStageMatches, shuffleArray } from "@/lib/utils";
+import { getPlayersByTeam } from "@/db/methods/player";
+import { validateTeam } from "@/lib/tournament-data";
+import { generateGroupStageMatches } from "@/lib/utils";
 import { CategoryFixture, Fixture, Team } from "@/shared/types";
+
+async function validateTeams(teams: Team[]) {
+  for (const team of teams) {
+    const players = await getPlayersByTeam(team.id);
+    const result = validateTeam(players);
+
+    if (!result.valid) {
+      return {
+        ok: false,
+        error: result.errors,
+      };
+    }
+  }
+
+  return {
+    ok: true,
+  };
+}
 
 export async function createMatchesAction(
   category: CategoryFixture,
   teams: Team[]
 ) {
+  const result = await validateTeams(teams);
+
+  if (result.ok === false) {
+    throw new Error(`${result.error}`);
+  }
+
   if (category.fixture.fixture_type === "groups") {
     const matches = generateGroupStageMatches(category.fixture as Fixture, [
       ...teams.map((team) => team.id),
