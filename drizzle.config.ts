07ccaec960a39bbc2bcdf27a3a7b20114ba11542
Gabriel Prado <gaboalespana@gmail.com>
2025-07-15 20:18:16 -0400

    update drizzle config to have two databases, one local and another on prod

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "drizzle-kit";
 
+const dialect = process.env.DB_FILE_NAME !== undefined ? "sqlite" : "turso";
+const dbCredentials =
+  dialect === "sqlite"
+    ? { url: process.env.DB_FILE_NAME! }
+    : {
+        url: process.env.TURSO_DATABASE_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN!,
+      };
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schemas/index.ts",
-  dialect: "turso",
-  dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-  },
+  dialect: dialect,
+  dbCredentials: dbCredentials,
 });
